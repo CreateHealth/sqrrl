890bb7ca95173275ec25bfc8da0d41c7078ec29f
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2017-06-21 10:33:21 -0400

    Documentation and upgrades to comparison ops
    - Export all underscore functions too
    - Docs and examples for everything
    - quotes() now detects type of individual elements in single vector
    - All operators escape column names

--- R/compare.R
+++ R/compare.R
@@ -1,44 +1,138 @@
-.delim  <- function(..., op) .delim_(names(c(...)), c(...), op)
-.delim_ <- function(x, y, op) paste(x, quotes(y), sep = op)
+#' Comparison Operators
+#'
+#' Generate SQL snippets comparing variables and values. `sqrrrl` provides two
+#' types of each function. Functions without an underscore, like `eq`, take an
+#' unlimited number of arguments and value pairs, where each argument is assumed
+#' to be single valued and the argument name is used as the column name.
+#' Functions with an underscore, like `eq_`, take a vector of columns and a
+#' vector of values that are zipped together. Infix operators are also provided
+#' for `LIKE` and `IN`.
+#'
+#' @name comparison
+NULL
 
+
+.delim  <- function(..., op) .delim_(names(c(...)), c(...), op)
+.delim_ <- function(var, val, op) paste(escape_col(var), quotes(val), sep = op)
+
+#' @rdname comparison
+#' @examples
+#' eq(a = 1, b = 'value')      # "a=1" "b=\"value\""
 #' @export
 eq     <- function(...) .delim(..., op = '=')
-eq_    <- function(x, y) .delim_(x, y, '=')
 
+#' @rdname comparison
+#' @examples
+#' eq_(letters[1:3], 1:3)      # "a=1" "b=2" "c=3"
+#'
+#' @export
+eq_    <- function(var, val) .delim_(var, val, '=')
+
+#' @rdname comparison
+#' @examples
+#' neq(a = 1, b = 'value')     # "a!=1" "b!=\"value\""
 #' @export
 neq    <- function(...) .delim(..., op = '!=')
-neq_ <- function(x, y) .delim_(x, y, '!=')
 
+#' @rdname comparison
+#' @examples
+#' neq_(letters[1:3], 1:3)     # "a!=1" "b!=2" "c!=3"
+#'
+#' @export
+neq_ <- function(var, val) .delim_(var, val, '!=')
+
+#' @rdname comparison
+#' @examples
+#' lt(a = 1, b = '2017-01-01') # "a<1" "b<\"2017-01-01\""
 #' @export
 lt <- function(...) .delim(..., op = '<')
-lt_ <- function(x, y) .delim_(x, y, op = '<')
 
+#' @rdname comparison
+#' @examples
+#' lt_(letters[1:3], 10:12)    # "a<10" "b<11" "c<12"
+#'
+#' @export
+lt_ <- function(var, val) .delim_(var, val, op = '<')
+
+#' @rdname comparison
+#' @examples
+#' leq(a = 1, b = '2017-01-01')# "a<=1" "b<=\"2017-01-01\""
 #' @export
 leq <- function(...) .delim(..., op = '<=')
-leq_ <- function(x, y) .delim_(x, y, op = '<=')
 
+#' @rdname comparison
+#' @examples
+#' leq_(letters[1:3], 10:12)   # "a<=10" "b<=11" "c<=12"
+#'
+#' @export
+leq_ <- function(var, val) .delim_(var, val, op = '<=')
+
+#' @rdname comparison
+#' @examples
+#' gt(a = 1, b = '2017-01-01') # "a>1" "b>\"2017-01-01\""
 #' @export
 gt <- function(...) .delim(..., op = '>')
-gt_ <- function(x, y) .delim_(x, y, op = '>')
 
+#' @rdname comparison
+#' @examples
+#' gt_(letters[1:3], 10:12)    # "a>10" "b>11" "c>12"
+#'
+#' @export
+gt_ <- function(var, val) .delim_(var, val, op = '>')
+
+#' @rdname comparison
+#' @examples
+#' geq(a = 1, b = '2017-01-01')# "a>=1" "b>=\"2017-01-01\""
 #' @export
 geq <- function(...) .delim(..., op = '>=')
-geq_ <- function(x, y) .delim_(x, y, op = '>=')
 
+#' @rdname comparison
+#' @examples
+#' geq_(letters[1:3], 10:12)   # "a>=10" "b>=11" "c>=12"
+#'
 #' @export
-in_ <- function(var, vals) paste(var, ifelse(length(vals) > 1, parens(quotes(vals)), parens(vals)), sep = ' IN ')
+geq_ <- function(var, val) .delim_(var, val, op = '>=')
 
+#' @rdname comparison
+#' @examples
+#' in_('a', c(5, 7, 8))        # "a IN (5, 7, 8)"
+#' @export
+in_ <- function(var, vals) {
+  var <- escape_col(var)
+  paste(var, ifelse(length(vals) > 1, parens(quotes(vals)), parens(vals)), sep = ' IN ')
+}
+
+#' @rdname comparison
+#' @examples
+#' 'a' %IN% c(5, 7, 8)         # "a IN (5, 7, 8)"
+#'
 #' @export
 `%IN%` <- function(var, vals) in_(var, vals)
 
+#' @rdname comparison
+#' @examples
+#' like_('a', 'Prefix%')       # "a LIKE(\"Prefix%\")"
 #' @export
-like_  <- function(var, val) paste0(var, ' LIKE("', val, '")')
+like_  <- function(var, val) paste0(escape_col(var), ' LIKE("', val, '")')
 
+#' @rdname comparison
+#' @examples
+#' 'a' %LIKE% 'Prefix%'        # "a LIKE(\"Prefix%\")"
 #' @export
 `%LIKE%` <- function(var, val) ifelse(length(val) > 1, like(var, val), like_(var, val))
 
+#' @rdname comparison
+#' @examples
+#' like('a', 'Prefix%', '%Suffix')
+#' # "(a LIKE(\"Prefix%\") OR a LIKE(\"%Suffix\"))"
+#'
 #' @export
-like   <- function(var, ...) parens(OR(sapply(c(...), like_, var = var)))
+like   <- function(var, ...) parens(OR(sapply(c(...), like_, var = escape_col(var))))
 
+#' @rdname comparison
+#' @examples
+#' BETWEEN('a', 0, 10)         # "a BETWEEN 0 AND 10"
+#' BETWEEN('timestamp', '2016-01-01', '2017-01-01')
+#' # "`timestamp` BETWEEN \"2016-01-01\" AND \"2017-01-01\""
 #' @export
-BETWEEN <- function(var, val1, val2) paste(var, 'BETWEEN', AND(quotes(val1, val2)))
+BETWEEN <- function(var, val1, val2) paste(escape_col(var), 'BETWEEN', AND(quotes(val1, val2)))
--- R/wrap.R
+++ R/wrap.R
@@ -12,14 +12,24 @@ backtick <- function(...) wrap('`', ...)
 #' @export
 quotes <- function(...) {
   items <- list(...)
-  unlist(lapply(items, function(x) {
-    is_number <- !any(is.na(suppressWarnings(as.numeric(x))))
-    if (is.factor(x)) quotes_(paste(x))
-    else if (inherits(x, 'Date')) quotes_(strftime(x, '%F'))
-    else if (inherits(x, 'POSIXt')) quotes_(strftime(x, '%F %T'))
-    else if (is_number) paste(x)
-    else quotes_(x)
-  }))
+  if (length(items) == 1 && length(items[[1]]) > 1) items <- as.list(items[[1]])
+  unlist(lapply(items, .quotes))
+}
+
+.quotes <- function(x, force = FALSE) {
+  if (force)
+    return(quotes_(x))
+  is_number <- !any(is.na(suppressWarnings(as.numeric(x))))
+  if (is.factor(x))
+    quotes_(paste(x))
+  else if (inherits(x, 'Date'))
+    quotes_(strftime(x, '%F'))
+  else if (inherits(x, 'POSIXt'))
+    quotes_(strftime(x, '%F %T'))
+  else if (is_number)
+    paste(x)
+  else
+    quotes_(x)
 }
 
 
